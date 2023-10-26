17d7d348f3689809c7210d493e7c77d1943121dd
gunawanrizqi <gunawanrizqi@gmail.com>
2023-10-26 23:25:57 +0700

    Edit first program

--- src/main/java/firstProgram/firstProgram.java
+++ src/main/java/firstProgram/firstProgram.java
@@ -3,5 +3,6 @@ package firstProgram;
 public class firstProgram {
     public static void main(String[] args) {
         System.out.println("Hello, World");
+        System.out.println("Hello, E!");
     }
 }
